7e45ebfe8d7275b538f7d27250b652333c4cdb88
Lukas Tetzlaff <tetzlaff@campus.tu-berlin.de>
2017-04-23 05:17:52 +0200

    :wrench: load babylon via npm

--- workspace/server.js
+++ workspace/server.js
@@ -4,10 +4,9 @@ const express = require("express")
 const e = express()
 const http = require("http").Server(e)
 
-const frontend = "./frontend"
-e.use(express.static(frontend))
+e.use(express.static("."))
 e.set("view engine", "pug")
-e.set("views", frontend)
+e.set("views", "frontend")
 e.get("/", (req, res) => res.render("ex1"))
 e.listen(3000)
 
