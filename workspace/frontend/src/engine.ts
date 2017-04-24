38efb8ae9447f5894b67835c0c289a183e2aa5bd
Lukas Tetzlaff <tetzlaff@campus.tu-berlin.de>
2017-04-24 02:41:20 +0200

    :rocket: instantiate box for every vertex closes #1

--- workspace/frontend/src/engine.ts
+++ workspace/frontend/src/engine.ts
@@ -1,6 +1,23 @@
 /// <reference path="../../node_modules/babylonjs/babylon.d.ts" />
-const B = BABYLON
-const V3 = BABYLON.Vector3
+
+const convertToPointCloud = (mesh : BABYLON.AbstractMesh, scene : BABYLON.Scene) => {
+  const vertices = mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind)
+      
+  if (vertices.length % 3 !== 0) {
+    throw new RangeError("Vertices array doesn't seem to consist of Vector3s")
+  }
+
+  const boxOptions = { size: .05 }
+  const vertCount = vertices.length / 3
+  const vertMeshes = new Array<BABYLON.Mesh>(vertCount)
+  for (let i = 0; i < vertCount; i++) {
+    let j = i * 3
+    vertMeshes[i] = BABYLON.MeshBuilder.CreateBox("v" + i, boxOptions, scene)
+    vertMeshes[i].setAbsolutePosition(new BABYLON.Vector3(vertices[j], vertices[j+1], vertices[j+2]))
+  }
+
+  scene.meshes.push(...vertMeshes) 
+}
 
 
 class Engine {
@@ -10,27 +27,27 @@ class Engine {
   private loader : BABYLON.AssetsManager
   private sun: BABYLON.Light
   private cam : BABYLON.ArcRotateCamera
-  private mesh : BABYLON.AbstractMesh
   private wfMat : BABYLON.Material
 
   constructor(canvas) {
     this.canvas = canvas
-    this.engine = new B.Engine(canvas, true)
+    this.engine = new BABYLON.Engine(canvas, true)
     this.engine.enableOfflineSupport = false
 
     // Setup Scene
-    this.scene = new B.Scene(this.engine)
+    this.scene = new BABYLON.Scene(this.engine)
     this.loader = new BABYLON.AssetsManager(this.scene)
-    this.cam = new B.ArcRotateCamera("Main Cam", -20, 0, 10, V3.Zero(), this.scene)
+    this.cam = new BABYLON.ArcRotateCamera("Main Cam", -20, 0, 10, BABYLON.Vector3.Zero(), this.scene)
     this.cam.attachControl(this.canvas, false)
-    this.sun = new B.HemisphericLight("Sun", new V3(0, 1, 0), this.scene)
+    this.sun = new BABYLON.HemisphericLight("Sun", new BABYLON.Vector3(0, 1, 0), this.scene)
 
     let mat = new BABYLON.StandardMaterial("wireframe", this.scene)
 	  mat.diffuseColor = BABYLON.Color3.Red()
+    mat.disableLighting = true
 	  mat.wireframe = true
     this.wfMat = mat
 
-    const ground = B.MeshBuilder.CreateGround("Ground", {
+    const ground = BABYLON.MeshBuilder.CreateGround("Ground", {
       width: 5, height: 5, subdivisions: 1
     }, this.scene)
   }
@@ -40,14 +57,20 @@ class Engine {
     this.engine.runRenderLoop(() => this.scene.render())
   }
 
-  load(file) : void {
+  load(file : string, asPointCloud : boolean = false) : void {
     BABYLON.SceneLoader.ImportMesh(file, "/models/", file, this.scene, meshes => { 
-      if (this.mesh) this.mesh.dispose()
-      this.mesh = meshes[0]
-      this.mesh.material = this.wfMat
+      // Remove old meshes
+      if (this.scene.meshes.length) this.scene.meshes = []
+      
+      if (asPointCloud) {
+        convertToPointCloud(meshes[0], this.scene)
+      } else {
+        this.scene.meshes.push(meshes[0])
+      }
+      
+      this.scene.meshes.forEach(mesh => mesh.material = this.wfMat)
     })
   }
-
 }
 
 var e : Engine
@@ -56,6 +79,3 @@ window.addEventListener("DOMContentLoaded", () => {
   e = new Engine(document.querySelector("#c") as HTMLCanvasElement)
   e.run()
 })
-
-
-
