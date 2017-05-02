499a3bd1654170d008dbc71fc6904ced52d0fa76
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2017-05-02 02:11:11 +0200

    :bug: model load path

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ e.set("view engine", "pug")
 e.set("views", ".")
 e.get("/", (req, res) => res.render("ex1"))
 e.get("/models/:file", (req, res) => {
-  readFile(join("..", "ex1", "off_files", req.params.file), "utf8", (err, data) => {
+  readFile(join(".", "ex1", "off_files", req.params.file), "utf8", (err, data) => {
     if (err) {
       console.error(err)
       res.sendStatus(500).end(err.toString())
