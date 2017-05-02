a009a446b35cc03e6d005fb8d475be58ddcf8163
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2017-05-02 02:12:04 +0200

    :rocket: octree visualization with UI Binding, limited Camera radius

--- frontend/engine.ts
+++ frontend/engine.ts
@@ -1,8 +1,10 @@
 import * as BABYLON from "../node_modules/babylonjs/babylon.module"
-
 import { Octree, Octant } from "./Trees/Octree"
+import "./OFFFileLoader"
 
-const convertToPointCloud = (mesh : BABYLON.AbstractMesh, scene : BABYLON.Scene) => {
+const globalOctreeOptions : any = {}
+
+const convertToPointCloud = (mesh : BABYLON.AbstractMesh, scene : BABYLON.Scene, mat : BABYLON.Material) => {
   const vertexCoordinates = mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind)
       
   if (vertexCoordinates.length % 3 !== 0) {
@@ -18,19 +20,19 @@ const convertToPointCloud = (mesh : BABYLON.AbstractMesh, scene : BABYLON.Scene)
     vertices[i] = new BABYLON.Vector3(vertexCoordinates[j], vertexCoordinates[j+1], vertexCoordinates[j+2])
     vertMeshes[i] = BABYLON.MeshBuilder.CreateBox("v" + i, boxOptions, scene)
     vertMeshes[i].setAbsolutePosition(vertices[i])
+    vertMeshes[i].material = mat
   }
 
   //scene.meshes.push(...vertMeshes) 
 
   // Create Octree
-  const octree = new Octree(vertices)
+  const octree = new Octree(vertices, globalOctreeOptions)
   const octreeVizMat = new WireFrameMaterial(BABYLON.Color3.Green(), scene)
-
 
   const visualize = (octant : Octant, scene : BABYLON.Scene) => {
     const s = octant.size
     const b = BABYLON.MeshBuilder.CreateBox("octant lv" + octant.level, 
-      { width: s[0], height: s[1], depth: s[2] }, scene)
+      { width: s.x, height: s.y, depth: s.z }, scene)
     b.setAbsolutePosition(octant.center)
     b.material = octreeVizMat
 
@@ -38,10 +40,9 @@ const convertToPointCloud = (mesh : BABYLON.AbstractMesh, scene : BABYLON.Scene)
     octant.children.forEach(child => visualize(child, scene))
   }
 
-  //visualize(octree, scene)
-  Object.defineProperty(window, "octree", { value: octree})
+  visualize(octree, scene)
+  Object.defineProperty(window, "octree", { value: octree}) // #debug
 }
-
 
 class Engine {
   private canvas : HTMLCanvasElement
@@ -61,6 +62,8 @@ class Engine {
     this.scene = new BABYLON.Scene(this.engine)
     this.loader = new BABYLON.AssetsManager(this.scene)
     this.cam = new BABYLON.ArcRotateCamera("Main Cam", -20, 0, 10, BABYLON.Vector3.Zero(), this.scene)
+    this.cam.upperRadiusLimit = 30
+    this.cam.lowerRadiusLimit = 2
     this.cam.attachControl(this.canvas, false)
     this.sun = new BABYLON.HemisphericLight("Sun", new BABYLON.Vector3(0, 1, 0), this.scene)
 
@@ -82,12 +85,11 @@ class Engine {
       if (this.scene.meshes.length) this.scene.meshes = []
       
       if (asPointCloud) {
-        convertToPointCloud(meshes[0], this.scene)
+        convertToPointCloud(meshes[0], this.scene, this.wfMat)
       } else {
+        meshes[0].material = this.wfMat
         this.scene.meshes.push(meshes[0])
-      }
-      
-      this.scene.meshes.forEach(mesh => mesh.material = this.wfMat)
+      }      
     })
   }
 }
@@ -96,14 +98,31 @@ class WireFrameMaterial extends BABYLON.StandardMaterial {
   constructor(color : BABYLON.Color3, scene : BABYLON.Scene) {
     super("wireframe", scene)
     this.diffuseColor = color
-    this.disableLighting = true
+    //this.disableLighting = true
 	  this.wireframe = true
   }
 }
 
-var e : Engine
+const $  = (selector : string) => document.querySelector(selector)
+const $$ = (selector : string) => document.querySelectorAll(selector)
 
+var e : Engine
 window.addEventListener("DOMContentLoaded", () => {
   e = new Engine(document.querySelector("#c") as HTMLCanvasElement)
   e.run()
+
+  // UI Bindings
+  const buttons = $$(".load").forEach((el : Element) => {
+    el.addEventListener("click", () => {
+      e.load(el.getAttribute("value") + ".off", true)
+    })
+  })
+
+  // Bind Octree-Configuration (see top) to ui elements
+  Object.defineProperty(globalOctreeOptions, "maxDepth", {
+    get: () => parseInt(($("#octreeMaxDepth") as HTMLInputElement).value)
+  })
+  Object.defineProperty(globalOctreeOptions, "bucketSize", {
+    get: () => parseInt(($("#octreeBucketSize") as HTMLInputElement).value)
+  })
 })
