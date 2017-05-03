4efffa2a42009eb813a5ede8e0d8bab6c7c85476
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2017-05-03 03:25:04 +0200

    :sparkles: switching to webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const { join, resolve } = require("path")
+const { sync } = require("glob")
+const { ContextReplacementPlugin } = require("webpack")
+const buildPath = join(__dirname, "built")
+
+const frontend = {
+  entry: "./frontend/engine.ts",
+  output: {
+    filename: "frontend.js",
+    path: buildPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /(node_modules)|(server)|(tests)/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+}
+
+const tests = {
+  entry: sync("./tests/**/*.ts"),
+  output: {
+    filename: "tests.js",
+    path: buildPath
+  },
+  node: {
+    fs: "empty"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /(node_modules)|(server)/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+}
+
+module.exports = [ frontend, tests]
