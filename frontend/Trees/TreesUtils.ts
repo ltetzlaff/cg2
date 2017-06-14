94eae4d9be589763702d052a65b1fc8c7670decb
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2017-06-14 02:27:25 +0200

    :rocket: introduce Vertex class

--- frontend/Trees/TreesUtils.ts
+++ frontend/Trees/TreesUtils.ts
@@ -1,5 +1,17 @@
 import { Vector3, Vector2, Ray, MathTools } from "../../node_modules/babylonjs/dist/preview release/babylon.module"
 
+export class Vertex {
+  public position : Vector3
+  public normal : Vector3
+  public index : number
+
+  constructor(position : Vector3, normal? : Vector3, index? : number) {
+    this.position = position
+    this.normal = normal
+    this.index = index
+  }
+}
+
 export namespace TreesUtils {
   export enum FindingPattern { KNearest, Radius }
 
